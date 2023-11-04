d599525945718695be3489433de29d66cd9a3bf2
Bruno Augusto <Brunoaug95@gmail.com>
2023-11-04 09:30:39 -0300

    Create migrations Product, Category

--- APICatalog/Models/Product.cs
+++ APICatalog/Models/Product.cs
@@ -2,5 +2,16 @@
 {
     public class Product
     {
+
+        public int ProductId {  get; set; }
+        public string? Name { get; set; }    
+        public string? Description { get; set; } 
+        public double Price { get; set; }
+        public string? UrlImage {  get; set; }
+        public float Stock {  get; set; }
+        public DateTime DataRegister {  get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
     }
 }
